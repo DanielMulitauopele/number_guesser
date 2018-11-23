e22c6a2bb51b0dbdde53cc4438e06f44c3795f8b
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-11-23 14:26:47 -0700

    working on using jquery in getting the guessing game to actually work

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.guess-button').on('click', function(){
+    var userGuess = $('#q')
+    var randNumber = 
+
+
+    const tooHighGuess = "That is too high!"
+    const tooLowGuess = "That is too low!"
+    const correctGuess = "BOOM!"
+
+
+  });
+});

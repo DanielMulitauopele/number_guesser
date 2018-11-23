a4dc7e7e5de0e0fef07c5389428faaba0abdf1e8
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-11-23 15:08:52 -0700

    adds functions for the user guess, as well as responses for too high, too low, correct. Adds css for user guess

--- scripts.js
+++ scripts.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
   $('.guess-button').on('click', function(){
-    var userGuess = $('#q')
-    var randNumber = 
-
+    var userGuess = $("#q")
+    const randNumber = Math.floor((Math.random() * 100) + 1);
 
     const tooHighGuess = "That is too high!"
     const tooLowGuess = "That is too low!"
     const correctGuess = "BOOM!"
 
+    if (userGuess.val() > randNumber) {
+      displayNumber(userGuess.val())
+      setMessage(tooHighGuess)
+      event.preventDefault();
+    } else if (userGuess.val() < randNumber) {
+      displayNumber(userGuess.val())
+      setMessage(tooLowGuess)
+      event.preventDefault();
+    } else {
+      displayNumber(userGuess.val())
+      setMessage(correctGuess)
+    }
 
+    function displayNumber(userGuess){
+      $('.guess').text(userGuess)
+    };
+
+    function setMessage(message){
+      $('.message').text(message)
+    };
   });
 });
